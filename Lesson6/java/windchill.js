1f37b7289df1ed7cae49ee2ba60ac4b879f037cd
emmaburkett <bur20089@byui.edu>
2021-02-14 00:26:11 -0700

    Github Page

--- Lesson6/java/windchill.js
+++ Lesson6/java/windchill.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', ()=> {
-                               
+    const temp = parseFloat(document.querySelector('#temp').innerHTML);
+    const windSpeed = parseFloat(document.querySelector('#speed').innerHTML);
+    const wind = document.querySelector('#windChill');
+
+    let windChill = 35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** .16)) + (.4275 * temp * (windSpeed ** .16));
+    let wC = windChill.toFixed(0)
+    wind.textContent = wC;
 });
